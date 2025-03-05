16849f52d15117dc34f366cf837711beb8a28d05
priteshparab <pritesh12@Pritesh.>
2025-03-05 03:52:46 +0000

    FIRST DNA SEQ SCRIPT
    FIRST DNA SEQ SCRIPT

---
+++ script.sh
@@ -0,0 +1,14 @@
+#Downloading the data
+
+#SRA Database : https://www.ncbi.nlm.nih.gov/sra/ERX2405312%5baccn%5d
+
+wget -c ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR235/009/ERR2356709/ERR2356709_1.fastq.gz
+wget -c ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR235/009/ERR2356709/ERR2356709_2.fastq.gz
+
+
+#Downloading reference genome
+
+#UCSC BROWSER : https://genome.ucsc.edu/
+#COMMAND:
+wget -c https://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/chr17.fa.gz
+
